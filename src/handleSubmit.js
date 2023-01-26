e7097e94051133ad50f2b874c0ea179f2810a81e
sjcyrn <joshua@cyrenian.me>
2023-01-26 02:00:46 -0800

    first build

--- src/handleSubmit.js
+++ src/handleSubmit.js
@@ -1,8 +1,9 @@
 import { addDoc, collection } from "@firebase/firestore"
 import { firestore } from "./firebase"
  
-const handleSubmit = (testdata) => {
-    console.log(testdata);
+const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event);
 //     const ref = collection(firestore, "test_data") // Firebase creates this automatically
 //  
 //     let data = {
